94496b91e0c8a2ba8c3d2e3a7fc3f3dca8828918
Scott Maclure <scott.maclure@sourcedgroup.com>
2021-06-12 19:25:30 +0800

    Setup roots display.

--- static/app.js
+++ static/app.js
@@ -4,6 +4,13 @@ import {render, html, svg} from 'https://unpkg.com/uhtml?module'
 // Contains the app - used for re-rendering.
 let mainElement = null
 
+// Global state
+let data = {
+    'roots': [],
+    'reservations': []
+}
+window.data = data // For browser console debuggging
+
 const getRoots = async () => {
     return fetch('/roots')
         .then(response => response.json())
@@ -12,19 +19,39 @@ const getRoots = async () => {
 const start = async (element) => {
     mainElement = element // set global for re-use on re-render.
     // Initial render
-    renderApp({
-        'roots': await getRoots()
-    })
+    data['roots'] = await getRoots()
+    renderApp()
 }
 
 /**
  * Top-down (re-)render.
+ * Note that micro html takes care of only replacing elements which change, so calling renderApp is cheap.
  * @param {*} data 
  */
-const renderApp = (data) => {
+const renderApp = () => {
     // console.log('renderApp.data', JSON.stringify(data, null, 2))
     console.log('renderApp.data', data)
-    render(mainElement, html`<p>Hello 👋 µhtml</p>`)
+    render(mainElement, html`
+        <p>Hello 👋 µhtml</p>
+        <nav class="tabs">
+            ${data.roots.map(
+                (root, i) => html`
+                    <a href="#!" class="${!!root.active ? 'active' : ''}" onclick=${() => setActiveRoot(root)}>
+                        (${root['pool_name']}) ${root['cidr']}
+                    </a>
+                `
+            )}
+        </nav>
+        <p>
+    `)
+}
+
+// TODO How about we pass uuids back for all items in db, then I can use metadata in state for stuf like "active tab".
+const setActiveRoot = (selectedRoot) => {
+    data.roots.forEach((root) => {
+        root.active = root.cidr === selectedRoot.cidr ? true : false
+    })
+    renderApp() // TODO Reactivity on data with get/set?
 }
 
 // Export
